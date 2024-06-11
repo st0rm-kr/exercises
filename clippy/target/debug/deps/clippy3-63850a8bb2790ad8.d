de1250a1e74daa113247d72b19b93c892569f7f4
kr <963689810@qq.com>
2024-06-12 03:16:53 +0800

    通过所有测试

---
+++ clippy/target/debug/deps/clippy3-63850a8bb2790ad8.d
@@ -0,0 +1,9 @@
+/home/kr/rustlings/exercises/clippy/target/debug/deps/libclippy3-63850a8bb2790ad8.rmeta: clippy3.rs Cargo.toml
+
+/home/kr/rustlings/exercises/clippy/target/debug/deps/clippy3-63850a8bb2790ad8.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
